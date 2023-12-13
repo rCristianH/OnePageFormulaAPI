f7676ea1c82739e9b97b0328d39003e47b1b5df3
Cristian Ramirez <ccrhdbj@outlook.com>
2023-12-13 17:24:19 -0500

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
 export default {
   base: 'https://rcristianh.github.io/OnePageFormulaAPI/', // Ajusta según tu repositorio
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
+    'import.meta.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY),
+  },
 }
